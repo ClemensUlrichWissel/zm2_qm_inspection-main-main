de73958445cbf7579121bc62848e9b96fc73d9cf
ClemensUlrichWissel <clemens.wissel@mindsquare.de>
2026-05-07 22:45:14 +0200

    sort function

--- zm2_qm_inspection-main/webapp/controller/Master.controller.js
+++ zm2_qm_inspection-main/webapp/controller/Master.controller.js
@@ -2,9 +2,13 @@ sap.ui.define([
 	"de/mindsquare/InspectionQM/controller/BaseController",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/Sorter"
+], function (BaseController, JSONModel, Filter, FilterOperator, Sorter) {
 	"use strict";
+
+	//Order in which active sort fields are applied to the list binding
+	const SORT_FIELD_ORDER = ["Werks", "Workcenter", "Material"];
 
 	return BaseController.extend("de.mindsquare.InspectionQM.controller.Master", {
 
@@ -13,8 +17,35 @@ sap.ui.define([
 			this.getView().setModel(new JSONModel({
 				plant: [],
 				workplace: [],
-				material: []
+				material: [],
+				sort: {
+					Werks: "",
+					Workcenter: "",
+					Material: ""
+				}
 			}), "FilterModel");
+		},
+
+		onToggleSort: function (oEvent) {
+			const oButton = oEvent.getSource();
+			const sField = oButton.data("sortField");
+			const oFilterModel = this.getView().getModel("FilterModel");
+			const sCurrent = oFilterModel.getProperty("/sort/" + sField);
+			//Cycle: none -> asc -> desc -> none
+			const sNext = sCurrent === "" ? "asc" : sCurrent === "asc" ? "desc" : "";
+
+			oFilterModel.setProperty("/sort/" + sField, sNext);
+			this._applySorters();
+		},
+
+		_applySorters: function () {
+			const oFilterModel = this.getView().getModel("FilterModel");
+			const oSortState = oFilterModel.getProperty("/sort");
+			const aSorters = SORT_FIELD_ORDER
+				.filter((sField) => oSortState[sField])
+				.map((sField) => new Sorter(sField, oSortState[sField] === "desc"));
+
+			this.getView().byId("idInspectionLotList").getBinding("items").sort(aSorters);
 		},
 
 		onSearch: function () {
